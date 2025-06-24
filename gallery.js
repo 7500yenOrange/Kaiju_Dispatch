6aaeb4efb21e271e9a1b84d18077f6273656778d
7500yenOrange <yamachan100ten@gmail.com>
2025-06-24 13:43:02 +0900

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -16,7 +16,7 @@ let contents = [
     },
     {
         title:"miComet",
-        caption:"fox＜ﾔｯﾊﾟ,miCometなんすね",
+        caption:"🦊🪧＜ﾔｯﾊﾟ,miCometなんすね",
         link:"https://x.com/DDyumi364/status/1934677257408467149",
         image:"Picture/miComet.jpg"
     }
